c81aa759eaae9889b261478d38c9d4fc80403f3d
Jason Antwi-Appah <hey@jasonaa.me>
2022-03-28 22:45:35 -0500

    chore: bump deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
 
   variants: {
